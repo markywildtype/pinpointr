57ca9804faa16518a74a471be63aba2b616eb164
Mark Blanford <markytorture@hotmail.com>
2018-05-10 15:46:29 +0100

    Address search returning coordinates correctly

--- app/query-form/query-form.component.js
+++ app/query-form/query-form.component.js
@@ -7,18 +7,17 @@ angular.module('queryForm').
       function QueryFormController($http){
         var self = this;
 
-        // $http.get('http://maps.google.com/maps/api/geocode/json?address=' + mapQuery).then(function(response){
-        $http.get('http://maps.google.com/maps/api/geocode/json?address=1+Orwell+Terrace,+Edinburgh').then(function(response){
-          var mapsData = response.data;
-          var coords = mapsData.results[0].geometry.location;
-          self.latitude = coords.lat;
-          self.longitude = coords.lng;
-        });
+        self.latitude = null;
+        self.longitude = null;
 
-        self.mapQuery = "";
-        //
-        // self.latitude = null;
-        // self.longitude = null;
+        self.getCoords = function(mapQuery){
+          $http.get('http://maps.google.com/maps/api/geocode/json?address=' + mapQuery).then(function(response){
+            var mapsData = response.data;
+            var coords = mapsData.results[0].geometry.location;
+            self.latitude = coords.lat;
+            self.longitude = coords.lng;
+          });
+        }
       }
     ]
   });
